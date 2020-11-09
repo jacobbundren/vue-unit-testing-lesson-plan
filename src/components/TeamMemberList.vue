357941f849a1f467ebea6ad6a477a3aac8985eb1
Jacob Bundren <jacobbundren@Jacobs-MacBook-Pro.local>
2020-11-09 14:33:47 -0600

    login and router

---
+++ src/components/TeamMemberList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TeamMemberList.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="home">
+        <div class="home__iconCircle">
+            <span class="material-icons home__bugIcon">bug_report</span>
+        </div>
+        <div class="home__title">
+            Buggy
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Home.vue"
+    }
+</script>
+
+<style scoped>
+.home {
+    margin-top: 80px;
+}
+.home__iconCircle {
+    height: 175px;
+    width: 175px;
+    border-radius: 50%;
+    background-color: #FF4139;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+.home__bugIcon {
+    font-size: 6.25em;
+    color: #FBF5F3;
+    transform: rotate(15deg);
+}
+.home__title {
+    font-size: 2.5em;
+    margin-top: .5em;
+}
+</style>
